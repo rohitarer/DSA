86159b1ed70b936a738caa06e5dd647f49f8b5cb
rohitarer <rohitarer00@gmail.com>
2025-08-13 10:06:05 +0530

    Linked List practice is going on

--- Java/LinkedLists.java
+++ Java/LinkedLists.java
@@ -8,147 +8,146 @@
 // Remove first
 // Remove last
 
-// public class LinkedLists {
-//     public static class Node {
-//         int data;
-//         Node next;
+public class LinkedLists {
+    public static class Node {
+        int data;
+        Node next;
 
-//         public Node(int data) {
-//             this.data = data;
-//             this.next = null;
-//         }
-//     }
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
 
-//     public static Node head;
-//     public static Node tail;
-//     public static int size;
+    public static Node head;
+    public static Node tail;
+    public static int size;
 
-//     public void addFirst(int data) {
-//         Node newNode = new Node(data);
-//         size++;
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        size++;
 
-//         if(head == null) {
-//             head = tail = newNode;
-//             return;
-//         }
+        if(head == null) {
+            head = tail = newNode;
+            return;
+        }
 
-//         newNode.next = head;
-//         head = newNode;
-//     }
+        newNode.next = head;
+        head = newNode;
+    }
 
-//     public void addLast(int data) {
-//         Node newNode = new Node(data);
-//         size++;
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        size++;
 
-//         if(head == null) {
-//             head = tail = newNode;
-//             return;
-//         }
+        if(head == null) {
+            head = tail = newNode;
+            return;
+        }
 
-//         tail.next = newNode;
-//         tail = newNode;
-//     }
+        tail.next = newNode;
+        tail = newNode;
+    }
 
-//     public void addMiddle(int idx, int data) {
-//         if(idx == 0) {
-//             addFirst(data);
-//             return;
-//         }
-//         Node newNode = new Node(data);
-//         size++;
-//         Node temp = head;
-//         int i =0;
+    public void addMiddle(int idx, int data) {
+        if(idx == 0) {
+            addFirst(data);
+            return;
+        }
+        Node newNode = new Node(data);
+        size++;
+        Node temp = head;
+        int i =0;
+        while(i < idx-1) {
+            temp = temp.next;
+            i++;
+        }
 
-//         while(i < idx-1) {
-//             temp = temp.next;
-//             i++;
-//         }
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
 
-//         newNode.next = temp.next;
-//         temp.next = newNode;
-//     }
+    public int removeFirst() {
+        if(size==0) {
+            System.out.println("LL is empty");
+            return Integer.MIN_VALUE;
+        } else if(size==1) {
+            int val = head.data;
+            head = tail = null;
+            size=0;
+            return val;
+        }
 
-//     public int removeFirst() {
-//         if(size==0) {
-//             System.out.println("LL is empty");
-//             return Integer.MIN_VALUE;
-//         } else if(size==1) {
-//             int val = head.data;
-//             head = tail = null;
-//             size=0;
-//             return val;
-//         }
+        int val = head.data;
+        head = head.next;
+        size--;
+        return val;
+    }
 
-//         int val = head.data;
-//         head = head.next;
-//         size--;
-//         return val;
-//     }
+    public int removeLast() {
+        if(size==0) {
+            System.out.println("LL is empty");
+            return Integer.MIN_VALUE;
+        } else if(size==1) {
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
 
-//     public int removeLast() {
-//         if(size==0) {
-//             System.out.println("LL is empty");
-//             return Integer.MIN_VALUE;
-//         } else if(size==1) {
-//             int val = head.data;
-//             head = tail = null;
-//             size = 0;
-//             return val;
-//         }
+        Node prev = head;
 
-//         Node prev = head;
+        for(int i=0; i<size-2; i++) {
+            prev = prev.next;
+        }
+        int val = prev.next.data;
+        prev.next = null;
+        tail = prev;
+        size--;
+        return val;
+    }
 
-//         for(int i=0; i<size-2; i++) {
-//             prev = prev.next;
-//         }
-//         int val = prev.next.data;
-//         prev.next = null;
-//         tail = prev;
-//         size--;
-//         return val;
-//     }
+    public void print() {
+        if(head == null) {
+            System.out.println("LL is empty");
+            return;
+        }
 
-//     public void print() {
-//         if(head == null) {
-//             System.out.println("LL is empty");
-//             return;
-//         }
+        Node temp = head;
 
-//         Node temp = head;
+        while(temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next; 
+        }
+        System.out.println("null");
+    }
 
-//         while(temp != null) {
-//             System.out.print(temp.data + "->");
-//             temp = temp.next; 
-//         }
-//         System.out.println("null");
-//     }
+    public static void main(String[] args) {
+        LinkedLists ll = new LinkedLists();
 
-//     public static void main(String[] args) {
-//         LinkedLists ll = new LinkedLists();
+        ll.print();
+        ll.addFirst(2);
+        ll.print();
+        ll.addFirst(1);
+        ll.print();
+        ll.addLast(4);
+        ll.print();
+        ll.addLast(5);
+        ll.print();
+        ll.addMiddle(2, 3);
+        ll.print();
 
-//         ll.print();
-//         ll.addFirst(2);
-//         ll.print();
-//         ll.addFirst(1);
-//         ll.print();
-//         ll.addLast(4);
-//         ll.print();
-//         ll.addLast(5);
-//         ll.print();
-//         ll.addMiddle(2, 3);
-//         ll.print();
+        // System.out.println("Size of LL: " + size);
 
-//         System.out.println("Size of LL: " + size);
+        // ll.removeFirst();
+        // ll.print();
+        // System.out.println("Size of LL: " + size);
 
-//         ll.removeFirst();
-//         ll.print();
-//         System.out.println("Size of LL: " + size);
-
-//         ll.removeLast();
-//         ll.print();
-//         System.out.println("Size of LL: " + size);
-//     }
-// }
+        // ll.removeLast();
+        // ll.print();
+        // System.out.println("Size of LL: " + size);
+    }
+}
 
 
 // 3. Search (Iterative)
@@ -1073,74 +1072,74 @@
 
 // 14. Doubly LL
 
-public class LinkedLists {
-    public static class Node {
-        int data;
-        Node next;
-        Node prev;
+// public class LinkedLists {
+//     public static class Node {
+//         int data;
+//         Node next;
+//         Node prev;
 
-        public Node(int data) {
-            this.data = data;
-            this.next = null;
-            this.prev = null;
-        }
-    }
+//         public Node(int data) {
+//             this.data = data;
+//             this.next = null;
+//             this.prev = null;
+//         }
+//     }
 
-    public static Node head;
-    public static Node tail;
-    public static int size;
+//     public static Node head;
+//     public static Node tail;
+//     public static int size;
 
-    public void addFirst(int data) {
-        Node newNode = new Node(data);
-        size++;
-        if(head == null) {
-            head = tail = newNode;
-            return;
-        }
+//     public void addFirst(int data) {
+//         Node newNode = new Node(data);
+//         size++;
+//         if(head == null) {
+//             head = tail = newNode;
+//             return;
+//         }
 
-        newNode.next = head;
-        head.prev = newNode;
-        head = newNode;
-    }
+//         newNode.next = head;
+//         head.prev = newNode;
+//         head = newNode;
+//     }
 
-    public int removeFirst() {
-        if(head == null) {
-            System.out.println("LL is empty");
-            return Integer.MIN_VALUE;
-        }
+//     public int removeFirst() {
+//         if(head == null) {
+//             System.out.println("LL is empty");
+//             return Integer.MIN_VALUE;
+//         }
 
-        if(size==1) {
-            int val = head.data;
-            head = tail = null;
-            return val;
-        }
+//         if(size==1) {
+//             int val = head.data;
+//             head = tail = null;
+//             return val;
+//         }
 
-        int val = head.data;
-        head = head.next;
-        head.prev = null;
-        return val;
-    }
+//         int val = head.data;
+//         head = head.next;
+//         head.prev = null;
+//         return val;
+//     }
 
-    public void print() {
-        Node temp = head;
+//     public void print() {
+//         Node temp = head;
 
-        while(temp!=null) {
-            System.out.print(temp.data + "<->");
-            temp = temp.next;
-        }
-        System.out.println("null");
-    }
+//         while(temp!=null) {
+//             System.out.print(temp.data + "<->");
+//             temp = temp.next;
+//         }
+//         System.out.println("null");
+//     }
 
-    public static void main(String[] args) {
-        LinkedLists ll = new LinkedLists();
-        ll.addFirst(3);
-        ll.addFirst(2);
-        ll.addFirst(1);
+//     public static void main(String[] args) {
+//         LinkedLists ll = new LinkedLists();
+//         ll.addFirst(3);
+//         ll.addFirst(2);
+//         ll.addFirst(1);
 
-        ll.print();
-        ll.removeFirst();
-        ll.print();
-        ll.removeFirst();
-        ll.print();
-    }
-}
+//         ll.print();
+//         ll.removeFirst();
+//         ll.print();
+//         ll.removeFirst();
+//         ll.print();
+//     }
+// }
---
+++ Java/tempCodeRunnerFile.java
@@ -0,0 +1,5 @@
+System.out.println("Size of LL: " + size);
+
+        // ll.removeLast();
+        // ll.print();
+        // System.out.println("Size of LL: " + size);
